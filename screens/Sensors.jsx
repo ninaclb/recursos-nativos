e09bf17734f22c5a2686620dd70a247d4dd11be8
Nina Carolina Lima Barater <ninacarolinabarater@gmail.com>
2023-08-14 16:47:13 -0300

    ta funcionado se atualizar

--- screens/Sensors.jsx
+++ screens/Sensors.jsx
@@ -1,7 +1,8 @@
-import { Text, View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Platform } from "react-native";
+import { Gyroscope, Magnetometer, Accelerometer, Barometer } from "expo-sensors";
+
 import Header from "../components/Header";
-import { Gyroscope, Magnetometer } from "expo-sensors";
-import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 
 const styles = StyleSheet.create({
@@ -33,6 +34,16 @@ const styles = StyleSheet.create({
     margin: 10,
     color: "#6750A4",
   },
+  button: {
+    padding: 10,
+    margin: 5,
+    backgroundColor: "#DDDDDD",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
 });
 
 export default function Sensors({ navigation }) {
@@ -40,26 +51,59 @@ export default function Sensors({ navigation }) {
   const [magneto, setMagneto] = useState({});
   const [orientation, setOrientation] = useState("Horizontal");
   const [backgroundColor, setBackgroundColor] = useState("#FFFFFF");
+  const [accelerometerData, setAccelerometerData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [barometerData, setBarometerData] = useState({
+    pressure: 0,
+    relativeAltitude: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const toggleAccelerometerListener = () => {
+    subscription ? unsubscribeAccelerometer() : subscribeAccelerometer();
+  };
+
+  const subscribeAccelerometer = () => {
+    setSubscription(Accelerometer.addListener(setAccelerometerData));
+  };
+
+  const unsubscribeAccelerometer = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  const _slowAccelerometer = () => Accelerometer.setUpdateInterval(1000); // atualiza a cada 1 segundo
+  const _fastAccelerometer = () => Accelerometer.setUpdateInterval(16); // atualiza a cada 16 milisegundos
 
   useEffect(() => {
-    Gyroscope.addListener(giroscopioListener);
-    Magnetometer.addListener(magnetoListener);
+    subscribeAccelerometer();
+    return () => unsubscribeAccelerometer();
+  }, []);
+
+  useEffect(() => {
+    const gyroscopeListener = (data) => {
+      setGiroscopio(data);
+      determineOrientation(data);
+    };
+
+    const magnetometerListener = (data) => {
+      setMagneto(data);
+    };
+
+    Gyroscope.addListener(gyroscopeListener);
+    Magnetometer.addListener(magnetometerListener);
+
     return () => {
       Gyroscope.removeAllListeners();
       Magnetometer.removeAllListeners();
     };
   }, []); // para atualizar
 
-  const giroscopioListener = (data) => {
-    setGiroscopio(data);
-    determineOrientation(data);
-  };
-  const magnetoListener = (data) => {
-    setMagneto(data);
-  };
-
   const determineOrientation = (gyroData) => {
-    const { x, y, z } = gyroData;
+    const { z } = gyroData;
     if (z > 1) {
       setOrientation("Vertical");
       setBackgroundColor("#FF0000");
@@ -72,36 +116,101 @@ export default function Sensors({ navigation }) {
     }
   };
 
+  const toggleBarometerListener = () => {
+    if (subscription) {
+      unsubscribeBarometer();
+    } else {
+      subscribeBarometer();
+    }
+  };
+
+  const subscribeBarometer = () => {
+    setSubscription(Barometer.addListener(({ pressure, relativeAltitude }) => {
+      setBarometerData({ pressure, relativeAltitude });
+    }));
+  };
+
+  const unsubscribeBarometer = () => {
+    if (subscription) {
+      subscription.remove();
+      setSubscription(null);
+    }
+  };
+
+  useEffect(() => {
+    subscribeBarometer();
+    return () => unsubscribeBarometer();
+  }, []);
+
   return (
     <View style={[styles.container, { backgroundColor }]}>
       <Header title={"Sensores"} style={styles.titulo} />
       <View style={styles.sensor}>
-        <Text style={styles.text}>
-          Giroscopio:{"\n"}
-          x: {giroscopio.x}
-          {"\n"}
-          y: {giroscopio.y}
-          {"\n"}
-          z: {giroscopio.z}
-          {"\n"}
-        </Text>
-        <Text style={styles.text}>
-          Magnetometro:{"\n"}
-          x: {magneto.x}
-          {"\n"}
-          y: {magneto.y}
-          {"\n"}
-          z: {magneto.z}
-          {"\n"}
-        </Text>
+        <ScrollView>
+          <Text style={styles.text}>
+            Giroscopio:{"\n"}
+            x: {giroscopio.x}
+            {"\n"}
+            y: {giroscopio.y}
+            {"\n"}
+            z: {giroscopio.z}
+            {"\n"}
+          </Text>
 
-        <Text style={styles.text}>Orientação: {orientation}</Text>
+          <Text style={styles.text}>
+            Magnetometro:{"\n"}
+            x: {magneto.x}
+            {"\n"}
+            y: {magneto.y}
+            {"\n"}
+            z: {magneto.z}
+            {"\n"}
+          </Text>
+
+          <Text style={styles.text}>Orientação: {orientation}</Text>
+
+          <Text style={styles.text}>
+            Acelerometro: (o gs quando 1g = 9.81 m/s^2)
+          </Text>
+
+          <Text style={styles.text}>
+            Acelerometro:
+            {"\n"}
+            x: {accelerometerData.x}
+            {"\n"}
+            y: {accelerometerData.y}
+            {"\n"}
+            z: {accelerometerData.z}
+            {"\n"}
+          </Text>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={subscription ? unsubscribeAccelerometer : subscribeAccelerometer}
+              style={styles.button}
+            >
+              <Text>{subscription ? "On" : "Off"}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={_slowAccelerometer} style={[styles.button, styles.middleButton]}>
+              <Text>Slow</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={_fastAccelerometer} style={styles.button}>
+              <Text>Fast</Text>
+            </TouchableOpacity>
+          </View>
+
+          <Text>Barometer: Listener {subscription ? 'ACTIVE' : 'INACTIVE'}</Text>
+          <Text>Pressure: {barometerData.pressure} hPa</Text>
+          <Text>
+            Relative Altitude:{' '}
+            {Platform.OS === 'ios' ? `${barometerData.relativeAltitude} m` : 'Only available on iOS'}
+          </Text>
+          <TouchableOpacity onPress={toggleBarometerListener} style={styles.button}>
+            <Text>Toggle listener</Text>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
-      <Footer
-        onPress={() => navigation.navigate("Home")}
-        title={"Voltar"}
-        style={styles.botao}
-      />
+      <Footer onPress={() => navigation.navigate("Home")} title={"Voltar"} style={styles.botao} />
     </View>
   );
 }
